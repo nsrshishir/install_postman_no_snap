00edc8b69706e200738d7571af001638ef3b7e90
Md Niaj Shahriar Shishir <73429060+nsrshishir@users.noreply.github.com>
2024-05-09 11:14:39 +0600

    Update install_postman_no_snap.sh
    overwrite while extracting

--- install_postman_no_snap.sh
+++ install_postman_no_snap.sh
@@ -3,7 +3,7 @@ wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz;
 
 echo "Installing Postman"
 echo "Extracting to /opt folder";
-sudo tar -xzf postman.tar.gz -C /opt;
+sudo tar -xzf --overwrite postman.tar.gz -C /opt;
 sudo ln -s /opt/Postman/Postman /usr/bin/postman;
 rm postman.tar.gz
 
@@ -17,7 +17,7 @@ Comment=Postman Desktop App
 Categories=Development;Code;
 EOF
 
-sudo mv ./postman.desktop /usr/share/applications/
+sudo mv -f./postman.desktop /usr/share/applications/
 
 echo "Instalation Successfull";
 exit
