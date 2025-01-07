d205816a9d4ae390cc8a4627186e2f90e58664d8
Md Niaj Shahriar Shishir <73429060+nsrshishir@users.noreply.github.com>
2025-01-07 12:50:47 +0600

    Update install_postman_no_snap.sh

--- install_postman_no_snap.sh
+++ install_postman_no_snap.sh
@@ -2,17 +2,17 @@ echo "Downloading latest postman";
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz;
 
 echo "Installing Postman"
-echo "Extracting to /opt folder";
-sudo tar -xzf --overwrite postman.tar.gz -C /opt;
-sudo ln -s /opt/Postman/Postman /usr/bin/postman;
+echo "Extracting to /usr/share folder";
+sudo tar -xzf postman.tar.gz -C /usr/share --overwrite;
+sudo ln -s /usr/share/Postman/Postman /usr/bin/postman;
 rm postman.tar.gz
 
 cat <<EOF > ./postman.desktop
 [Desktop Entry]
 Type=Application
 Name=Postman
-Icon=/opt/Postman/app/resources/app/assets/icon.png
-Exec="/opt/Postman/Postman"
+Icon=/usr/share/Postman/app/resources/app/assets/icon.png
+Exec="/usr/share/Postman/Postman"
 Comment=Postman Desktop App
 Categories=Development;Code;
 EOF
